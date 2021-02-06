fa6d5184101dfe8c93121de9453a653adc4fc097
ElvisLee <elvislee0725@gmail.com>
2021-02-05 17:33:48 -0800

    run client and server concurrently

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Landing from './components/layout/Landing';
+
+const App = () => 
+  <Fragment>
+    <Landing />
+  </Fragment>
+
+export default App;
