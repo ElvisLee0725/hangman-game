0ad651060eb3657faaaea135a93cdc562a5b4e44
ElvisLee <elvislee0725@gmail.com>
2021-04-10 15:34:58 -0700

    Add scores page

--- client/src/components/layout/TopScores.js
+++ client/src/components/layout/TopScores.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Table } from 'react-bootstrap';
+import axios from 'axios';
 
 const TopScores = () => {
+  const [topScores, setTopScores] = useState([]);
+  
+  const fetchScores = async () => {
+    try {
+      const scores = await axios.get('/api/scores');
+      console.log(scores.data);
+      setTopScores([...scores.data]);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchScores();
+  }, []);
+
   return (
-    <div>
-      scores!
-    </div>
+    <Container>
+      <h1 className='text-center my-5'>Top 10 Scores</h1>
+      <Table striped variant='dark'>
+        <thead className='table-header'>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody className='table-text'>
+          { topScores.map((s, i) => (<tr key={i}><td>{i+1}</td><td>{s.name}</td><td>{s.score}</td></tr>))}
+        </tbody>
+      </Table>
+    </Container>
   );
 }
 
