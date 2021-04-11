e14e07a56c1f62b46081137f171162021341de4d
ElvisLee <elvislee0725@gmail.com>
2021-04-10 17:55:40 -0700

    Create player name input for Winner modal

--- client/src/components/layout/WinnerModal.js
+++ client/src/components/layout/WinnerModal.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
 
 const WinnerModal = (props) => {
-  
+  const [playerName, setPlayerName] = useState('');
+
   return (
     // Use animation={false} to remove Warning:findDOMNode is deprecated in StrictMode. However, modal loses animation
     <Modal {...props} animation={false} centered>
       <Modal.Header closeButton>
-        <Modal.Title>You got the answer!</Modal.Title>
+        <Modal.Title>Nice! You got the answer!</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Your total score: {props.score}</h4>
-        <label>
-          Enter your name:
-          <input></input>
-        </label>
+        <h4>Total Score: {props.score}</h4>
+        <Form>
+          <Form.Group as={Row} controlId="formPlayerName">
+            <Form.Label column sm={4}>
+              Your Name:
+            </Form.Label>
+            <Col sm={8}>
+              <Form.Control type="text" name="playerName" value={playerName} onChange={(e) => setPlayerName(e.target.value)}></Form.Control>
+            </Col>
+          </Form.Group>
+          <div className="save-name-buttons">
+            <Button variant="secondary" onClick={props.onHide} className="mr-2">Close</Button>
+            <Button variant="primary" onClick={props.onHide}>Save</Button>
+          </div>
+        </Form>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={props.onHide}>Close</Button>
-        <Button variant="primary" onClick={props.onHide}>Save</Button>
-      </Modal.Footer>
     </Modal>
   );
 }
