d1dd16977b85240766d6692b15792cf9e9fcd30c
ElvisLee <elvislee0725@gmail.com>
2021-04-24 21:46:26 -0700

    Add favicon and logo

--- client/src/components/layout/Navigation.js
+++ client/src/components/layout/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = () => {
   return (
     <Fragment>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Hangman</Navbar.Brand>
+        <Navbar.Brand><Link to='/'><img alt="Hangman-Logo" src="/hangman-logo.png" height="40"></img></Link></Navbar.Brand>
         <Nav className="mr-auto">
           <Link to='/' className='nav-link'>
             Game Board
