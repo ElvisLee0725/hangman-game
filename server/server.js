3e6ca0c2ada8d4bd420107ec126a7c881a5fe701
ElvisLee <elvislee0725@gmail.com>
2021-05-08 15:47:48 -0700

    modify code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('../config/db');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -10,13 +11,11 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('API Running');
+  res.send(`API Running on ${PORT}`);
 });
 
 app.use('/api/questions', require('../routes/api/questions'));
 app.use('/api/scores', require('../routes/api/scores'));
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening to PORT ${PORT}`);
